abadfe727e3c7ac990210daaf12221d495c78218
nrakochy <nick.rakochy@gmail.com>
2016-02-11 11:52:28 -0600

    Switched from Webkit (Qt dependency) to Poltergeist (phantom.js dependency)

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
   gem 'capybara'
   gem 'rspec'
-  gem 'capybara-webkit'
+  gem 'poltergeist'
 
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,16 +2,13 @@ require 'rspec'
 require 'capybara'
 require 'capybara/dsl'
 require 'capybara/rspec'
-require 'capybara-webkit'
+require 'capybara/poltergeist'
 require 'support/helper_methods'
 require 'support/constants'
 
-Capybara.javascript_driver = :webkit
+Capybara.default_driver = :poltergeist
+Capybara.javascript_driver = :poltergeist
 Capybara.app_host = URL_UNDER_TEST
-
-Capybara::Webkit.configure do |config|
-  config.allow_url(URL_UNDER_TEST)
-end
 
 RSpec.configure do |config|
   config.include Capybara::DSL
